a2e763dbe0d24972c071342f03b3f9896a07c021
Thomas Karpiniec <tom.karpiniec@outlook.com>
2025-07-23 21:24:39 +1000

    Remove local buildscripts

--- buildscripts/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-cd "$(git rev-parse --show-toplevel)"
-source buildscripts/init.sh "$1"
-
-cargo clippy --all-targets --target "${RUST_TARGET}" -- -D warnings
-cargo fmt --all --check
